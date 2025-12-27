2775f4f24c3b87e089cab42619be7513e0e49781
EfficiencySimplicity <joshua.ward@fastmail.com>
2025-12-27 07:56:04 -0500

    capitalization of paths

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const { scriptApp } = require(path.resolve(__dirname, 'public/projects/sitesilike/serverScripts.js'));
+const { scriptApp } = require(path.resolve(__dirname, 'public/projects/sitesILike/serverScripts.js'));
 
 const IP_ADDRESS = process.env.IP || '::';
 const PORT = process.env.PORT || 8100;
